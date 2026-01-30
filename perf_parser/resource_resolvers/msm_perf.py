b5450195771be694adefa618ad10e9b0b4939fb3
Arian <arian.kulmer@web.de>
2026-01-30 17:14:10 +0100

    fixup! Implement msm_perf value oddities

--- perf_parser/resource_resolvers/msm_perf.py
+++ perf_parser/resource_resolvers/msm_perf.py
@@ -2,6 +2,16 @@ from perf_parser.models import ResourceContext, ResolvedPair
 from perf_parser.utils.cpu import get_cpus_for_cluster, get_next_available_frequency_for_cluster
 from typing import Iterable
 
+
 def resolve_msm_perf(ctx: ResourceContext) -> Iterable[ResolvedPair]:
-    value = get_next_available_frequency_for_cluster(ctx.target_info, ctx.cluster, 1000 * ctx.raw_value)
-    return [(ctx.node, " ".join(f"{cpu}:{value}" for cpu in get_cpus_for_cluster(ctx.target_info, ctx.cluster)))]
+    value = get_next_available_frequency_for_cluster(
+        ctx.target_info, ctx.cluster, 1000 * ctx.raw_value
+    )
+    return [
+        (
+            ctx.node,
+            ' '.join(
+                f'{cpu}:{value}' for cpu in get_cpus_for_cluster(ctx.target_info, ctx.cluster)
+            ),
+        )
+    ]
